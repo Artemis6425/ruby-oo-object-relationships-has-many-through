2734d4ada907ed454320f04bdc81e932e02fe687
Artemis6425 <aurorarose6425@gmail.com>
2020-11-15 20:25:34 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
 class Customer
-  
+    @@all = []
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+    attr_accessor :name, :age
+    def self.all
+        @@all
+    end
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter,self,total,tip)
+    end
+    def meals
+        Meal.all.select do |temp|
+            temp.customer == self
+        end
+    end
+    def waiters
+        meals.map do |temp|
+            temp.waiter
+        end
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
-
+    @@all = []
+    def initialize(name, xp)
+        @name = name
+        @experience = xp
+        @@all << self
+    end
+    attr_accessor :name, :experience
+    def self.all
+        @@all
+    end
+    def new_meal(customer, total, tip=0)
+        Meal.new(self,customer,total,tip)
+    end
+    def meals
+        Meal.all.select do |temp|
+            temp.waiter == self
+        end
+    end
+    def best_tipper
+        best_tip = meals.max do |a, b|
+            a.tip <=> b.tip
+        end
+        best_tip.customer
+    end
 end
